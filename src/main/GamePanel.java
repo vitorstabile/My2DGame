087363bf93b3ae8e3c275f4c41d1e767313bb49f
Vitor Garcia <vitor.stabile@hotmail.com>
2025-05-10 17:12:25 +0100

    Add Retang in screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -34,9 +34,33 @@ public class GamePanel extends JPanel implements Runnable {
 
         while(gameThread != null) {
 
-            System.out.println("The game loop is running");
+            //System.out.println("The game loop is running");
+
+            // 1 UPDATE: update information such as character positions
+            update();
+
+            // 2 DRAW: draw the screen with the updated information
+            repaint();
 
         }
+
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
 
     }
 }
