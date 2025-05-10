b4d3e45a4e56d238198928b94c233c5ad82fde4d
Vitor Garcia <vitor.stabile@hotmail.com>
2025-05-10 19:26:56 +0100

    Remove Retang, add Player Image and load images to Panel

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -16,6 +19,7 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultKeys();
+        getPlayerImage();
 
     }
 
@@ -24,21 +28,45 @@ public class Player extends Entity {
         x=100;
         y=100;
         speed=4;
+        direction = "down";
+
+    }
+
+    public void getPlayerImage(){
+
+        try {
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void update() {
 
         if(keyH.upPressed){
+            direction = "up";
             y -= speed;
         }
         else if(keyH.downPressed){
+            direction = "down";
             y += speed;
         }
         else if(keyH.leftPressed){
+            direction = "left";
             x -= speed;
         }
         else if(keyH.rightPressed){
+            direction = "right";
             x += speed;
         }
 
@@ -46,10 +74,27 @@ public class Player extends Entity {
 
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.white);
+//        g2.setColor(Color.white);
+//
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
 
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 
     }
 }
