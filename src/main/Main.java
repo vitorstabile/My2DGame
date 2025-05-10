5eb350213f55e5af68d56b4d02ca7fef8a76e58e
Vitor Garcia <vitor.stabile@hotmail.com>
2025-05-10 16:46:06 +0100

    Created the GamePanel

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,10 @@ public class Main {
         window.setResizable(false);
         window.setTitle("2D Adventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
