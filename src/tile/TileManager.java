461d6685dbe646d652381f2cd56ffb9ed4d0492e
Vitor Garcia <vitor.stabile@hotmail.com>
2025-05-18 13:32:01 +0100

    Add more tiles in a inefficient way

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -43,6 +43,8 @@ public class TileManager {
     public void draw(Graphics2D g2) {
 
         g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[1].image, 48, 0, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[2].image, 96, 0, gp.tileSize, gp.tileSize, null);
 
     }
 
